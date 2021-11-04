292cb47833fa8d977d92fca71b1e25c99d7d0190
Joopjr <mail@joopjr.nl>
2021-11-04 16:30:18 +0100

    There is no data send if player is on an unsupported world (deadman, beta, seasonal our tournament)

--- src/main/java/com/runelogger/RuneloggerPlugin.java
+++ src/main/java/com/runelogger/RuneloggerPlugin.java
@@ -5,7 +5,10 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
 import net.runelite.api.Player;
+import net.runelite.api.WorldType;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
@@ -40,6 +43,7 @@ public class RuneloggerPlugin extends Plugin
 	@Inject private ApiCommunication apiCommunication;
 
 	private boolean characterInfoSend = false;
+	private boolean suportedWorld = false;
 
 	//PLUGIN STARTED//
 	@Override
@@ -64,37 +68,57 @@ public class RuneloggerPlugin extends Plugin
 		"You feel something weird sneaking into your backpack");
 	 */
 
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		//GAME CHANGED TO USER LOGGED IN
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		{
+			//PLAYER ISN'T ON AN UNSUPORTED WORLD
+			if(!client.getWorldType().contains(WorldType.DEADMAN) && !client.getWorldType().contains(WorldType.NOSAVE_MODE) && !client.getWorldType().contains(WorldType.SEASONAL) && !client.getWorldType().contains(WorldType.TOURNAMENT_WORLD))
+				suportedWorld = true;
+		}
+	}
+
 	//EXECUTE EVERY GAME TICK//
 	@Subscribe
 	public void onGameTick(GameTick tick)
 	{
-		//CHECK IF PLAYER NAME IS NOT NULL
-		if(client.getLocalPlayer().getName() != null && !characterInfoSend) {
-			//CLONE PLAYER OBJECT
-			Player player = client.getLocalPlayer();
+		//PLAYER IS ON A SUPORTED WORLD
+		if(suportedWorld)
+		{
+			//CHECK IF PLAYER NAME IS NOT NULL
+			if(client.getLocalPlayer().getName() != null && !characterInfoSend) {
+				//CLONE PLAYER OBJECT
+				Player player = client.getLocalPlayer();
 
-			//SEND THE CHARACTER INFO TO THE API
-			characterInfoSend = apiCommunication.sendCharacterInfo();
+				//SEND THE CHARACTER INFO TO THE API
+				characterInfoSend = apiCommunication.sendCharacterInfo();
+			}
+
+			if(config.skillingBosses()) skillingBosses.gametickSkillingBoss();		//SKILLING BOSSES ARE ENABLED
+			if(config.levelingUp()) levelingUp.gametickLevelingUp();				//LEVELING UP IS ENABLED
+			if(config.questCompletion()) questCompletion.gametickQuestCompletion();	//QUEST COMPLETION IS ENABLED
+			if(config.tutorialIsland()) tutorialIsland.gametickTutorialIsland();	//TUTORIAL ISLAND IS ENABLED
+			if(config.duelArena()) duelArena.gametickDuelArena();					//DUEL ARENA IS ENABLED
 		}
-
-		if(config.skillingBosses()) skillingBosses.gametickSkillingBoss();		//SKILLING BOSSES ARE ENABLED
-		if(config.levelingUp()) levelingUp.gametickLevelingUp();				//LEVELING UP IS ENABLED
-		if(config.questCompletion()) questCompletion.gametickQuestCompletion();	//QUEST COMPLETION IS ENABLED
-		if(config.tutorialIsland()) tutorialIsland.gametickTutorialIsland();	//TUTORIAL ISLAND IS ENABLED
-		if(config.duelArena()) duelArena.gametickDuelArena();					//DUEL ARENA IS ENABLED
 	}
 
 	@Subscribe
 	public void onChatMessage(ChatMessage chatMessage)
 	{
-        String message = chatMessage.getMessage();
+		//PLAYER IS ON A SUPORTED WORLD
+		if(suportedWorld)
+		{
+			String message = chatMessage.getMessage();
 
-		if(config.skillingBosses()) skillingBosses.chatSkillingBoss(message);						//SKILLING BOSSES ARE ENABLED
-		if(config.musicUnlocks()) musicUnlock.chatMusicUnlock(message);								//MUSIC UNLOCKS ARE ENABLED
-		if(config.collectionLog()) collectionLog.chatCollectionLog(message);						//COLLECTION LOG IS ENABLED
-        if(config.bosses()) bosses.chatBosses(message);												//BOSS KILLS ARE ENABLED
-		if(config.clueScrollCompletion()) clueScrollCompletion.chatClueScrollCompletion(message);	//CLUE SCROLL COMPLETIONS ARE ENABLED
-		if(config.soloCombatMinigame()) soloCombatMinigame.chatSoloCombatMinigame(message);				//COMBAT CHALLENGES ARE ENABLED
+			if(config.skillingBosses()) skillingBosses.chatSkillingBoss(message);						//SKILLING BOSSES ARE ENABLED
+			if(config.musicUnlocks()) musicUnlock.chatMusicUnlock(message);								//MUSIC UNLOCKS ARE ENABLED
+			if(config.collectionLog()) collectionLog.chatCollectionLog(message);						//COLLECTION LOG IS ENABLED
+			if(config.bosses()) bosses.chatBosses(message);												//BOSS KILLS ARE ENABLED
+			if(config.clueScrollCompletion()) clueScrollCompletion.chatClueScrollCompletion(message);	//CLUE SCROLL COMPLETIONS ARE ENABLED
+			if(config.soloCombatMinigame()) soloCombatMinigame.chatSoloCombatMinigame(message);			//COMBAT CHALLENGES ARE ENABLED
+		}
 	}
 
 	@Provides
